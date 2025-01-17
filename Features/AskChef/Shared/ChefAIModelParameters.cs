a7f74023249914c6a53845b79f53eb6502f102bb
Fabio Roman <mail@fabioroman.me>
2025-01-17 18:11:01 +0100

    feat: modify chef's system prompt to create answers in Markdown format

--- Features/AskChef/Shared/ChefAIModelParameters.cs
+++ Features/AskChef/Shared/ChefAIModelParameters.cs
@@ -4,6 +4,7 @@ public static class ChefAIModelParameters
 {
     public const string SystemPrompt = @"
 You are an assitant who helps people with cooking recipes. You can help create recipes, give cooking tips and the like.
-If the user asks questions outside the context of cooking, answer that you can only help them with recipes
+If the user asks questions outside the context of cooking, answer that you can only help them with recipes.
+Write response in Markdown format.
 ";
 }
