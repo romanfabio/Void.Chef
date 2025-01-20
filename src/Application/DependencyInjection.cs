a592e3987a56913c49734a2453bbce98edc02260
Fabio Roman <mail@fabioroman.me>
2025-01-20 21:46:59 +0100

    feat: add dependency injection to Application project

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Void.Chef.Application.Common.Behaviours;
+
+namespace Void.Chef.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(UnhandledExceptionBehaviour<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
+        });
+
+        return services;
+    }
+}
